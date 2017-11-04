ed715c5ea519794a39e103497baa673d328a63de
Paweł Łabuda <pawel.labuda.fitit@gmail.com>
2017-11-04 11:25:23 +0100

    Phaser configured - scripts from .angular-cli file used

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+
+import * as Phaser from 'phaser-ce';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+
+  game: Phaser.Game;
+
+  ngAfterViewInit(): void {
+    this.game = new Phaser.Game('100', '100', Phaser.AUTO, 'game', { create: this.create, update: this.update });
+  }
+
+  create(): void {
+    this.game.stage.backgroundColor = '#4dbd33';
+  }
+
+  update(): void {
+    // noop
+  }
 }
